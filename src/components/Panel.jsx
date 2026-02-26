3ab7b27b8c01bc572ed3963506c801e1685b328d
geraldx2005 <gerald6742@gmail.com>
2026-02-26 17:03:45 +0530

    renamed the header

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -13,7 +13,7 @@ const Panel = () => {
         className="w-full sm:w-[50%] md:w-[40%] lg:w-[33%] xl:w-[28%] h-screen bg-ner0-800"
       >
         <div className='w-full bg-nero-800 text-nero-300 text-xl px-4 py-2'>
-          Configuration
+          Chronos Layout Planner
         </div>
 
         <div className="minimal-scrollbar overflow-y-auto overflow-x-hidden w-full h-[calc(100vh-44px)] flex flex-col justify-start items-center bg-nero-800 border-t-2 border-nero-900 text-nero-300">
